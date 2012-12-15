abc863e9966aacb0bcb01a099f5080a1d12bb19f
Chris Linstid <clinstid@gmail.com>
2012-12-14 21:21:14 -0500

    Switch to square class to drive stuff.

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,25 @@
+// The board is going to look like this, with the squares as numbered:
+//
+//    |   |
+//  0 | 1 | 2
+// -----------
+//    |   |
+//  3 | 4 | 5
+// -----------
+//  6 | 7 | 8
+//    |   |
+
+// Our canvas that we're drawing on and an eventual context to use for it
 var canvas;
 var context;
 
+// Our message box's location. The y coordinate for the box is going to be
+// updated once we get our canvas's dimensions.
 var message_x = 5;
-
-// The y coordinate of the message is going to change, but we'll start with a
-// reasonable default value.
 var message_y = 480;
-var message_y_offset = 20;
+var message_height = 30;
 
+// The width to use for the lines on the board
 var board_line_width = 2;
 
 var vert_left_start_x = 0;
@@ -36,17 +48,45 @@ var horiz_bottom_end_y = 0;
 
 var click_count = 0;
 
-var SQUARE_EMPTY = 0;
-var SQUARE_X = 1;
-var SQUARE_O = 2;
+var squares = new Array();
 
-function draw_board(name) {
+function Square(start_x, start_y, end_x, end_y)
+{
+    var SQUARE_EMPTY = 0;
+    var SQUARE_X = 1;
+    var SQUARE_O = 2;
+
+    this.start_x = start_x;
+    this.start_y = start_y;
+    this.end_x = end_x;
+    this.end_y = end_y;
+    this.contains = SQUARE_EMPTY;
+
+}
+
+Square.prototype.within = function(x, y, border_width)
+{
+    if (x > (this.start_x + border_width) && x < (this.end_x - border_width) &&
+        y > (this.start_y + border_width) && y < (this.end_y - border_width))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+function draw_board(name) 
+{
     canvas = document.getElementById(name);
 
-    message_y = canvas.height - message_y_offset;
+    message_y = canvas.height - message_height;
 
     // Check the element is in the DOM and the browser supports canvas
-    if(canvas.getContext) {
+    if(canvas.getContext) 
+    {
         // Initialize a 2-dimensional drawing context
         context = canvas.getContext('2d');
 
@@ -59,66 +99,46 @@ function draw_board(name) {
         //
         // 0,0    x/3,0 2x/3,0 x,0
         //           |      |
-        //           |      |
+        //         0 |  1   | 2
         // 0,y/3  -------------- x,y/3
         //           |      |
-        //           |      |
+        //         3 |  4   | 5
         // 0,2y/3 -------------- x,2y/3
-        //           |      |
+        //         6 |  7   | 8
         //           |      |
         // 0,y    x/3,y 2x/3,y x,y
         //
 
         x = canvas.width;
-        y = canvas.height - 100;
+        y = canvas.height - message_height;
 
-        vert_left_start_x = x / 3;
-        vert_left_start_y = 0;
+        square[0] = new Square(0, 0, x/3, y/3);
+        square[1] = new Square(x/3, 0, 2*x/3, y/3);
+        square[2] = new Square(2*x/3, 0, x, y/3);
 
-        vert_left_end_x = vert_left_start_x;
-        vert_left_end_y = y;
+        square[3] = new Square(0, y/3, x/3, 2*y/3);
+        square[4] = new Square(x/3, y/3, 2*x/3, 2*y/3);
+        square[5] = new Square(2*x/3, y/3, x, 2*y/3);
 
-        vert_right_start_x = 2 * x / 3;
-        vert_right_start_y = 0;
-
-        vert_right_end_x = vert_right_start_x;
-        vert_right_end_y = y;
-
-        horiz_top_start_x = 0;
-        horiz_top_start_y = y / 3;
-
-        horiz_top_end_x = x;
-        horiz_top_end_y = horiz_top_start_y;
-
-        horiz_bottom_start_x = 0;
-        horiz_bottom_start_y = 2 * y / 3;
-
-        horiz_bottom_end_x = x;
-        horiz_bottom_end_y = horiz_bottom_start_y;
+        square[6] = new Square(0, 2*y/3, x/3, y);
+        square[7] = new Square(x/3, 2*y/3, 2*x/3, y);
+        square[8] = new Square(2*x/3, 2*y/3, x, y);
 
         // left vertical line
-        draw_line(vert_left_start_x, 
-                  vert_left_start_y,
-                  vert_left_end_x,
-                  vert_left_end_y);
+        draw_line(square[1].start_x, square[1].start_y,
+                  square[6].end_x, square[6].end_y);
 
         // right vertical line
-        draw_line(vert_right_start_x, 
-                  vert_right_start_y,
-                  vert_right_end_x,
-                  vert_right_end_y);
+        draw_line(square[2].start_x, square[2].start_y,
+                  square[7].end_x, square[7].end_y);
 
         // top horizontal line
-        draw_line(horiz_top_start_x, 
-                  horiz_top_start_y,
-                  horiz_top_end_x,
-                  horiz_top_end_y);
+        draw_line(square[3].start_x, square[3].start_y,
+                  square[2].end_x, square[2].end_y);
 
         // bottom horizontal line
-        draw_line(horiz_bottom_start_x, 
-                  horiz_bottom_start_y,
-                  horiz_bottom_end_x,
-                  horiz_bottom_end_y);
+        draw_line(square[6].start_x, square[6].end_y,
+                  square[5].end_x, square[5].end_y);
 
         spacing = 5;
 
@@ -136,7 +156,8 @@ function draw_board(name) {
     }
 }
 
-function draw_line(start_x, start_y, end_x, end_y) {
+function draw_line(start_x, start_y, end_x, end_y) 
+{
         context.strokeStyle = "gray";
         context.lineWidth = board_line_width;
 
@@ -147,7 +168,8 @@ function draw_line(start_x, start_y, end_x, end_y) {
         context.stroke();
 }
 
-function draw_x(start_x, start_y, end_x, end_y) {
+function draw_x(start_x, start_y, end_x, end_y) 
+{
         context.strokeStyle = "#000000";
         context.lineWidth = 3;
 
@@ -164,7 +186,8 @@ function draw_x(start_x, start_y, end_x, end_y) {
         context.stroke();
 }
 
-function draw_o(start_x, start_y, end_x, end_y) {
+function draw_o(start_x, start_y, end_x, end_y) 
+{
         context.strokeStyle = "#000000";
         context.lineWidth = 3;
 
@@ -179,14 +202,16 @@ function draw_o(start_x, start_y, end_x, end_y) {
         context.stroke();
 }
 
-function write_message(message) {
+function write_message(message) 
+{
     context.clearRect(message_x, message_y, canvas.width, canvas.height);
     context.font = "12pt Monospace";
     context.fillStyle = "black";
     context.fillText(message, message_x, message_y);
 }
 
-function track_mouse(event) {
+function track_mouse(event) 
+{
     var rect = canvas.getBoundingClientRect();
     //console.log("Mouse position: %d,%d", 
     //            event.clientX - rect.left, 
@@ -194,7 +219,8 @@ function track_mouse(event) {
 
 }
 
-function mouse_click(event) {
+function mouse_click(event) 
+{
     click_count += 1;
     console.log("Mouse click: %d,%d #%d",
                 event.x - canvas.offsetLeft,
